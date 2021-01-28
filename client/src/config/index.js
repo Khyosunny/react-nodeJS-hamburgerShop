d5142b171be5d0699a98fd2a99b86b422b907f2c
Khyosunny <kimhyo_0218@naver.com>
2021-01-28 22:21:23 +0900

    주소변경

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,8 @@
-export const USER_SERVER = 'http://localhost:5000/api/users'
-export const PRODUCT_SERVER = 'http://localhost:5000/api/product'
-export const IMAGE_SERVER = 'http://localhost:5000/'
+// export const USER_SERVER = 'http://localhost:5000/api/users'
+// export const PRODUCT_SERVER = 'http://localhost:5000/api/product'
+// export const IMAGE_SERVER = 'http://localhost:5000/'
+export const USER_SERVER =
+  'https://hyosun-react-hamburger-shop.herokuapp.com/api/users'
+export const PRODUCT_SERVER =
+  'https://hyosun-react-hamburger-shop.herokuapp.com/api/product'
+export const IMAGE_SERVER = 'https://hyosun-react-hamburger-shop.herokuapp.com/'
